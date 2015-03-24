5c788da9ef6f6e738324beace17890929c4a72ee
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-03-24 11:00:45 +0000

    Changed CurrencyValues from a SortedList to an enumerable of Tuples

--- Code/RomanNumerals.cs
+++ Code/RomanNumerals.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Code
@@ -9,13 +10,12 @@ namespace Code
         {
             var map = new Dictionary<string, int>();
 
-            CurrencyValues.OrderBy(kvp => kvp.Key)
-                .Reverse()
+            CurrencyValues
                 .ToList()
-                .ForEach(kvp =>
+                .ForEach(tuple =>
                 {
-                    map.Add(kvp.Value, n/kvp.Key);
-                    n = n%kvp.Key;
+                    map.Add(tuple.Item2, n/tuple.Item1);
+                    n = n%tuple.Item1;
                 });
 
             return map.Aggregate(
@@ -28,40 +28,39 @@ namespace Code
             var n = 0;
 
             CurrencyValues
-                .Where(c => c.Value.Length > 1)
+                .Where(tuple => tuple.Item2.Length > 1)
                 .ToList()
-                .ForEach(c =>
+                .ForEach(tuple =>
                 {
-                    if (s.IndexOf(c.Value) >= 0)
+                    if (s.IndexOf(tuple.Item2) >= 0)
                     {
-                        n += c.Key;
-                        s = s.Replace(c.Value, string.Empty);
+                        n += tuple.Item1;
+                        s = s.Replace(tuple.Item2, string.Empty);
                     }
                 });
 
             var remainingTotal =
                 (from romanChar in s
-                    join cv in CurrencyValues.ToList() on romanChar.ToString() equals cv.Value
-                    select cv.Key).Sum();
+                    join tuple in CurrencyValues.ToList() on romanChar.ToString() equals tuple.Item2
+                    select tuple.Item1).Sum();
 
             return n + remainingTotal;
         }
 
-        private static readonly SortedList<int, string> CurrencyValues = new SortedList<int, string>
-        {
-            {1000, "M"},
-            {900, "CM"},
-            {500, "D"},
-            {400, "CD"},
-            {100, "C"},
-            {90, "XC"},
-            {50, "L"},
-            {40, "XL"},
-            {10, "X"},
-            {9, "IX"},
-            {5, "V"},
-            {4, "IV"},
-            {1, "I"}
+        private static readonly Tuple<int, string>[] CurrencyValues = {
+            Tuple.Create(1000, "M"),
+            Tuple.Create(900, "CM"),
+            Tuple.Create(500, "D"),
+            Tuple.Create(400, "CD"),
+            Tuple.Create(100, "C"),
+            Tuple.Create(90, "XC"),
+            Tuple.Create(50, "L"),
+            Tuple.Create(40, "XL"),
+            Tuple.Create(10, "X"),
+            Tuple.Create(9, "IX"),
+            Tuple.Create(5, "V"),
+            Tuple.Create(4, "IV"),
+            Tuple.Create(1, "I")
         };
     }
 }
